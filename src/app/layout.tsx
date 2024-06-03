8e3136697aa1bfec6f6ff32dc22cab562daa0c8f
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-06-03 14:41:51 +0200

    refactor: remove head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,17 @@ import { useTitle, TitleProvider } from './providers/TitleContext';
 import Head from 'next/head';
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ['latin'] });
 
 const LayoutContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { title } = useTitle();
   return (
     <div className={`flex flex-row overflow-hidden ${inter.className}`}>
-      <Head>
-        <title>{title}</title>
-      </Head>
       <NavBar />
       <div className="flex flex-col py-8 px-6 bg-gray-100 overflow-y-auto w-full space-y-6">
-        <header className="text-2xl text-gray-700">{title}</header>
+        <h1 className="text-2xl text-gray-700">{title}</h1>
+        <header></header>
         <main className="flex flex-grow">{children}</main>
       </div>
     </div>
