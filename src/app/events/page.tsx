f20b2adba00fe10fb6d82296564282d32090f58f
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-06-03 15:44:12 +0200

    refactor: put a useEffect on the setTitle

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import Image from "next/image";
 import { useTitle } from "../providers/TitleContext";
+import { useEffect } from "react";
 
 export default function Home() {
   const {setTitle} = useTitle()
-  setTitle('Events')
+  useEffect(() => {
+    setTitle('Events')
+  })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       event
