2bf61140b04011277eb303d844044f6658d7b662
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-06-04 21:36:44 +0200

    feat: fix nav-bar size

--- src/app/components/molecules/nav-bar.tsx
+++ src/app/components/molecules/nav-bar.tsx
@@ -5,7 +5,6 @@ import NavItem from "../atoms/nav-item";
 import SideBarButton from "../atoms/sidebar-button";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
-import { Cross2Icon } from '@radix-ui/react-icons'
 
 const navItems = [
     {
@@ -27,7 +26,7 @@ export default function NavBar () {
     const toggleOpen = () => setIsOpen(!isOpen)
     const path = usePathname()
     return(
-        <nav className={`flex ${isOpen ? "bg-background absolute w-full h-full" : 'static h-fit'}  md:relative md:w-fit flex-col px-6 py-4 md:py-8 space-y-16`}>
+        <nav className={`flex ${isOpen ? "bg-background absolute w-full h-full" : 'static h-fit'}  md:relative md:w-fit flex-col px-6 py-4 md:py-8 space-y-16 z-20`}>
             <section className={`flex flex-col space-y-4`}>
                 <div className="flex w-full justify-between">
                     <SideBarButton className={`md:absolute -right-4`} isOpen={isOpen} onClick={toggleOpen}/>
