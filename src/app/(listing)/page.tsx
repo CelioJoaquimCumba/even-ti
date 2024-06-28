c3df202cabc2653a7b346b97206866ea60de6adb
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-06-28 14:43:22 +0200

    feat: add loading and show something if there is no result

--- src/app/(listing)/page.tsx
+++ src/app/(listing)/page.tsx
@@ -7,6 +7,7 @@ import { EventLite } from "@/data/types";
 import dataWaveEvent from '@/../assets/images/datawave-event.png'
 import profile from '/@/../assets/images/profile.png'
 import { convertDate } from "@/lib/utils";
+import { EventCardLoader } from "../components/molecules/event-card-loader";
 
 const eventsDummy: Array<EventLite> = [
   {
@@ -31,7 +32,7 @@ const eventsDummy: Array<EventLite> = [
         image: profile.src
       }
     ]
-  }, 
+  },
   {
     id: '2',
     community: "MozDevz",
@@ -58,32 +59,40 @@ const eventsDummy: Array<EventLite> = [
 ]
 
 export default function Home() {
-  const [events, setEvents] = useState<EventLite[]>(eventsDummy)
+  const [isLoading, setIsLoading] = useState(true)
+  const [events, setEvents] = useState<EventLite[]>()
   const {setTitle} = useTitle()
   useEffect(() => {
     setTitle('Events')
   })
   useEffect(() => {
     (async function () {
-      const events = await fetch(`/api/event`, {
-        method: "GET"
-      });
-      const data = await events.json();
-      const responseEvents : EventLite[] = data.map((event: any) => ({
-        ...event,
-        date: convertDate(event.date.toString()),
-        speakers: event.speakers.map((speaker: any) => ({
-          id: speaker.speaker.id,
-          name: speaker.speaker.name,
-          image: speaker.speaker.image
+      try {
+        setIsLoading(true)
+        const events = await fetch(`/api/event`, {
+          method: "GET"
+        });
+        const data = await events.json();
+        const responseEvents : EventLite[] = data.map((event: any) => ({
+          ...event,
+          date: convertDate(event.date.toString()),
+          speakers: event.speakers.map((speaker: any) => ({
+            id: speaker.speaker.id,
+            name: speaker.speaker.name,
+            image: speaker.speaker.image
+          }))
         }))
-      }))
-      setEvents(responseEvents)
+        setEvents(responseEvents)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
     })()
   }, [])
   return (
     <main className="flex w-full h-full flex-col items-center gap-2 md:gap-6 bg-white rounded-2xl overflow-y-auto">
-      {events.map((event: EventLite) => <EventCard key={event.id} event={event} />)}
+      {isLoading ? [1,2].map((_event, index) => <EventCardLoader key={index} />)  : !events ? 'Nao temos eventos para mostrar' : events.map((event: EventLite) => <EventCard key={event.id} event={event} />)}
     </main>
   );
 }
