437907f13538ce3167c7d272c6b82dd346690093
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-07-09 00:15:54 +0200

    fix: add user to navbar profile item

--- src/app/components/molecules/nav-bar.tsx
+++ src/app/components/molecules/nav-bar.tsx
@@ -5,36 +5,66 @@ import NavItem from '../atoms/nav-item'
 import SideBarButton from '../atoms/sidebar-button'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
+import { routes } from '@/data/routes'
+import { isBreakpointLowOrEqual } from '@/utils'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { EnterIcon, ExitIcon } from '@radix-ui/react-icons'
+import { Button } from '../atoms/button'
 
 const navItems = [
   {
     label: 'Eventos',
-    path: '/events',
+    path: routes.events,
   },
   {
     label: 'Comunidade',
-    path: '/community',
+    path: routes.community,
   },
   {
     label: 'Reservas',
-    path: '/reservations',
+    path: routes.reservations,
   },
 ]
 
-export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(true)
+export default function SideBar() {
+  const [isOpen, setIsOpen] = useState(!!isBreakpointLowOrEqual('md'))
   const toggleOpen = () => setIsOpen(!isOpen)
   const path = usePathname()
+  const { user } = useUser()
   return (
-    <nav className="flex relative flex-col px-6 py-8 space-y-16">
-      <SideBarButton
-        className="absolute -right-4 "
-        isOpen={isOpen}
-        onClick={toggleOpen}
-      />
-      <section className={`flex flex-col space-y-4 ${!isOpen && 'hidden'}`}>
-        <div className="bg-gray-200 w-24 h-10 rounded-lg"></div>
-        <ProfileNavItem />
+    <aside
+      className={`flex ${isOpen ? 'bg-background absolute w-full h-full' : 'static h-fit'}  md:relative md:w-fit flex-col px-6 py-4 md:py-8 space-y-16 z-20`}
+    >
+      <section className={`flex flex-col space-y-4`}>
+        <div className="flex w-full justify-between">
+          <SideBarButton
+            className={`md:absolute -right-4`}
+            isOpen={isOpen}
+            onClick={toggleOpen}
+          />
+          <div
+            className={`bg-gray-200 w-24 h-10 rounded-lg ${!isOpen && 'hidden'}`}
+          ></div>
+        </div>
+        <div className={`${!isOpen && 'hidden'} flex flex-col items-center`}>
+          {user ? (
+            <>
+              <ProfileNavItem user={user} />
+              <a href="/api/auth/logout">
+                <Button variant={'outline'} className="rounded-full">
+                  <ExitIcon /> Terminar Sessão
+                </Button>
+              </a>
+            </>
+          ) : (
+            <a href="/api/auth/login">
+              <Button variant={'default'} className="rounded-full">
+                Autenticar-se
+                <EnterIcon />
+              </Button>
+            </a>
+          )}
+        </div>
       </section>
       <ul
         className={`flex flex-col space-y-4 overflow-y-auto ${!isOpen && 'hidden'}`}
@@ -48,6 +78,6 @@ export default function NavBar() {
           />
         ))}
       </ul>
-    </nav>
+    </aside>
   )
 }
