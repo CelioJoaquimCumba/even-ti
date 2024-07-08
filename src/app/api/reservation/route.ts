1c7b683c8bdee48922a04f59803cd832b6adb00d
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-07-08 22:55:36 +0200

    feat: get reservations based on the user

--- src/app/api/reservation/route.ts
+++ src/app/api/reservation/route.ts
@@ -8,12 +8,17 @@ import prisma from '@/lib/prisma'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: NextRequest, _res: NextApiResponse) {
   // const search = req.nextUrl.searchParams.get('search') || ''
+  const userId = req.nextUrl.searchParams.get('userId')
+  if (!userId) return
   const page = Number(req.nextUrl.searchParams.get('page')) || 1
   const pageSize = 10
   const totalCount = await prisma.reservation.count()
   const skip = (page - 1) * pageSize
   const take = pageSize
   const reservationsWithSpeakers = await prisma.reservation.findMany({
+    where: {
+      userId,
+    },
     include: {
       event: true,
     },
