12a1f9ea0ac6fb17afdb75a7bbf95591b6ffcd87
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-07-08 15:51:15 +0200

    refactor: remove console log

--- src/app/(listing)/reservations/page.tsx
+++ src/app/(listing)/reservations/page.tsx
@@ -38,7 +38,6 @@ export default function Home() {
           },
         )
         const data = await response.json()
-        console.log(data)
         setMeta({
           totalCount: data.totalCount,
           page: data.page,
