a51e87823ba12f41c5f6eb12f0e12146dacd6a98
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-07-10 18:58:19 +0200

    feat: make event reservation modal

--- src/app/components/molecules/event-card.tsx
+++ src/app/components/molecules/event-card.tsx
@@ -11,6 +11,7 @@ import { SpeakerCard } from '../atoms/speaker-card'
 import { EventLite } from '@/data/types'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import ReserveEventModal from './reserve-event-modal'
 
 export function EventCard(props: { event: EventLite }) {
   const {
@@ -27,106 +28,120 @@ export function EventCard(props: { event: EventLite }) {
   } = props.event
   const [isOpen, setIsOpen] = useState(false)
   const router = useRouter()
+  const [showModal, setShowModal] = useState(false)
+
+  const toggleModal = () => setShowModal(!showModal)
   return (
-    <Card
-      className={`flex flex-col space-y-2 ${!isOpen && 'md:flex-row'} w-full hover:bg-gray-50 justify-between p-4 rounded-2xl gap-2 cursor-pointer`}
-      onClick={() => router.push(`/events/${id}`)}
-    >
-      <CardContent className="flex flex-wrap md:flex-nowrap gap-6 p-0 ">
-        <div
-          className={`hidden ${!isOpen && 'md:flex md:flex-col'} w-40 h-40 justify-center items-center border rounded-3xl`}
-        >
-          <h2>{date.split('de')[1]}</h2>
-          <h3>{date.split('de')[0]}</h3>
-        </div>
-        <Image
-          unoptimized
-          src={image}
-          alt="datawave-event"
-          width={200}
-          height={200}
-          className={`flex flex-col ${isOpen ? 'w-80 aspect-[3/4]' : 'w-40 h-40 aspect-square bg-cover'} justify-center items-center border rounded-3xl`}
-        />
-        <section className="flex flex-col space-y-2">
-          <div className="flex justify-between w-full">
-            <h2 className="text-base font-normal">{community}</h2>
+    <>
+      <Card
+        className={`flex flex-col space-y-2 ${!isOpen && 'md:flex-row'} w-full hover:bg-gray-50 justify-between p-4 rounded-2xl gap-2 cursor-pointer`}
+        onClick={() => router.push(`/events/${id}`)}
+      >
+        <CardContent className="flex flex-wrap md:flex-nowrap gap-6 p-0 ">
+          <div
+            className={`hidden ${!isOpen && 'md:flex md:flex-col'} w-40 h-40 justify-center items-center border rounded-3xl`}
+          >
+            <h2>{date.split('de')[1]}</h2>
+            <h3>{date.split('de')[0]}</h3>
+          </div>
+          <Image
+            unoptimized
+            src={image}
+            alt="datawave-event"
+            width={200}
+            height={200}
+            className={`flex flex-col ${isOpen ? 'w-80 aspect-[3/4]' : 'w-40 h-40 aspect-square bg-cover'} justify-center items-center border rounded-3xl`}
+          />
+          <section className="flex flex-col space-y-2">
+            <div className="flex justify-between w-full">
+              <h2 className="text-base font-normal">{community}</h2>
+              <Button
+                variant={'outline'}
+                size={'icon'}
+                className={`rounded-full hidden md:flex ${!isOpen && 'md:hidden'}`}
+                onClick={(e) => {
+                  setIsOpen(!isOpen)
+                  e.stopPropagation()
+                }}
+              >
+                <ChevronUp />
+              </Button>
+            </div>
+            <h2 className="text-lg font-medium">{title}</h2>
+            <ul
+              className={`flex flex-col ${isOpen ? 'md:flex-row md:flex-wrap' : 'md:flex-col'} gap-2`}
+            >
+              <Badge
+                variant={'secondary'}
+                className={`gap-2 px-2 py-1 w-fit ${!isOpen && 'md:hidden'}`}
+              >
+                <CalendarIcon />
+                <span className="md:whitespace-nowrap">{date}</span>
+              </Badge>
+              <Badge variant={'secondary'} className="gap-2 px-2 py-1 w-fit">
+                <ClockIcon />
+                <span className="md:whitespace-nowrap">{time}</span>
+              </Badge>
+              <Badge variant={'secondary'} className="gap-2 px-2 py-1 w-fit">
+                <SewingPinIcon />
+                <span className="md:whitespace-nowrap">{location}</span>
+              </Badge>
+            </ul>
+            <p className={`${isOpen && 'md:block'} hidden text-primary`}>
+              {description}
+            </p>
+            <section className={`hidden flex-col ${isOpen && 'md:flex'}`}>
+              <h3>Speakers:</h3>
+              <div className="flex gap-2 flex-wrap">
+                {speakers.map((speaker) => (
+                  <SpeakerCard key={speaker.id} speaker={speaker} />
+                ))}
+              </div>
+            </section>
+          </section>
+        </CardContent>
+        <CardFooter className={`flex flex-col justify-between items-end p-0`}>
+          <Button
+            variant={'outline'}
+            size={'icon'}
+            className={`rounded-full hidden md:flex ${isOpen && 'md:hidden'}`}
+            onClick={(e) => {
+              setIsOpen(!isOpen)
+              e.stopPropagation()
+            }}
+          >
+            <ChevronDown />
+          </Button>
+          <div className="flex gap-2 w-full">
+            <Link
+              href={`/event/${props.event.id}`}
+              className={`w-full md:w-fit whitespace-pre-line hidden ${isOpen && 'md:flex'}`}
+            >
+              <Button
+                variant={'outline'}
+                className="w-full md:w-fit whitespace-pre-line px-8 py-4"
+              >
+                Ver mais detalhes
+              </Button>
+            </Link>
             <Button
-              variant={'outline'}
-              size={'icon'}
-              className={`rounded-full hidden md:flex ${!isOpen && 'md:hidden'}`}
+              variant={'default'}
+              className="w-full md:w-fit whitespace-pre-line px-8 py-4"
               onClick={(e) => {
-                setIsOpen(!isOpen)
+                toggleModal()
                 e.stopPropagation()
               }}
             >
-              <ChevronUp />
+              Reservar Bilhete {tickets && `(${tickets} disponiveis)`}
             </Button>
           </div>
-          <h2 className="text-lg font-medium">{title}</h2>
-          <ul
-            className={`flex flex-col ${isOpen ? 'md:flex-row md:flex-wrap' : 'md:flex-col'} gap-2`}
-          >
-            <Badge
-              variant={'secondary'}
-              className={`gap-2 px-2 py-1 w-fit ${!isOpen && 'md:hidden'}`}
-            >
-              <CalendarIcon />
-              <span className="md:whitespace-nowrap">{date}</span>
-            </Badge>
-            <Badge variant={'secondary'} className="gap-2 px-2 py-1 w-fit">
-              <ClockIcon />
-              <span className="md:whitespace-nowrap">{time}</span>
-            </Badge>
-            <Badge variant={'secondary'} className="gap-2 px-2 py-1 w-fit">
-              <SewingPinIcon />
-              <span className="md:whitespace-nowrap">{location}</span>
-            </Badge>
-          </ul>
-          <p className={`${isOpen && 'md:block'} hidden text-primary`}>
-            {description}
-          </p>
-          <section className={`hidden flex-col ${isOpen && 'md:flex'}`}>
-            <h3>Speakers:</h3>
-            <div className="flex gap-2 flex-wrap">
-              {speakers.map((speaker) => (
-                <SpeakerCard key={speaker.id} speaker={speaker} />
-              ))}
-            </div>
-          </section>
-        </section>
-      </CardContent>
-      <CardFooter className={`flex flex-col justify-between items-end p-0`}>
-        <Button
-          variant={'outline'}
-          size={'icon'}
-          className={`rounded-full hidden md:flex ${isOpen && 'md:hidden'}`}
-          onClick={(e) => {
-            setIsOpen(!isOpen)
-            e.stopPropagation()
-          }}
-        >
-          <ChevronDown />
-        </Button>
-        <div className="flex gap-2 w-full">
-          <Link
-            href={`/event/${props.event.id}`}
-            className={`w-full md:w-fit whitespace-pre-line hidden ${isOpen && 'md:flex'}`}
-          >
-            <Button
-              variant={'outline'}
-              className="w-full md:w-fit whitespace-pre-line px-8 py-4"
-            >
-              Ver mais detalhes
-            </Button>
-          </Link>
-          <Button
-            variant={'default'}
-            className="w-full md:w-fit whitespace-pre-line px-8 py-4"
-          >
-            Reservar Bilhete {tickets && `(${tickets} disponiveis)`}
-          </Button>
-        </div>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+      <ReserveEventModal
+        open={showModal}
+        close={toggleModal}
+        event={props.event}
+      />
+    </>
   )
 }
---
+++ src/app/components/molecules/reserve-event-modal.tsx
@@ -0,0 +1,49 @@
+import { EventLite } from '@/data/types'
+import { Button } from '../atoms/button'
+import Image from 'next/image'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './dialog'
+export default function ReserveEventModal(props: {
+  open: boolean
+  close: () => void
+  event: EventLite
+}) {
+  const { open, event } = props
+  const { title, description, logo, date, time } = event
+  return (
+    <Dialog open={open} onOpenChange={props.close}>
+      <DialogContent className="sm:max-w-[425px] md:p-8">
+        <DialogHeader className="flex flex-col md:gap-6">
+          <DialogTitle>Deseja participar do evento {title}?</DialogTitle>
+          <DialogDescription className="flex flex-col gap-2 md:gap-6">
+            <Image
+              unoptimized
+              src={logo}
+              alt="datawave-event"
+              width={200}
+              height={200}
+              className={`flex flex-col max-w-md md:w-full aspect-[4/3] bg-cover justify-center items-center border rounded-3xl`}
+            />
+            <p className="text-base font-normal text-slate-700">
+              {description}
+            </p>
+            <p className="text-base font-normal text-slate-700">{date}</p>
+            <p className="text-base font-normal text-slate-700">{time}</p>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex flex-wrap gap-2">
+          <Button variant={'outline'} onClick={props.close}>
+            Cancelar
+          </Button>
+          <Button variant={'default'}>Reservar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
