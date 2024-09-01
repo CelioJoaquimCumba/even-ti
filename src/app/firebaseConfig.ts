0831cba184ee2f7c49ba87805be76b74c5504ab9
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-09-01 21:41:01 +0200

    feat: remove firebase analytics

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
 import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 // Get a reference to the storage service
 const storage = getStorage(app)
-const analytics = getAnalytics(app)
-export { storage, analytics }
+export { storage }
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,36 +1,45 @@
 'use client'
 import { usePage } from '@/app/providers/PageContext'
 import { useEffect, useState } from 'react'
-import SettingsProfileCard from '../components/molecules/settings/settings-profile-card'
+import SettingsProfileCard from '@/app/components/molecules/settings/settings-profile-card'
 import { User } from '@/data/types'
-import { getUserById } from '../actions/user'
+import { getUserById } from '@/app/actions/user'
 import { useUser } from '@auth0/nextjs-auth0/client'
 
 export default function SettingsPage() {
   const { setTitle } = usePage()
-  const [userData, setUserData] = useState<User>()
+  const [userData, setUserData] = useState<User | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
   const { user } = useUser()
+
   useEffect(() => {
     setTitle('Definições')
-  })
+  }, [setTitle])
+
   useEffect(() => {
     ;(async function () {
       if (!user) return
       try {
         setLoading(true)
         const response = await getUserById(
-          (user && user.sub && user.sub.toString().replace('auth0|', '')) || '',
+          (user.sub && user.sub.toString().replace('auth0|', '')) || '',
         )
-        if (!response) return
+        if (!response) throw new Error('User not found')
         setUserData(response)
+      } catch (err) {
+        setError('Algo de errado não está certo')
+        console.error(err)
       } finally {
         setLoading(false)
       }
     })()
   }, [user])
-  if (loading) return <>Loading...</>
-  if (!userData) return <>Algo de errado não está certo</>
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>{error}</div>
+  if (!userData) return <div>Algo de errado não está certo</div>
+
   return (
     <div className="flex w-full h-full flex-col gap-2 md:gap-6 bg-white rounded-2xl overflow-y-auto">
       <SettingsProfileCard user={userData} />
