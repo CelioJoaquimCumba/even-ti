fc5d68936798011df79a567e288cc426cdbe7c1f
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-09-07 10:50:56 +0200

    refactor: make lint fix

--- src/app/components/atoms/textarea.tsx
+++ src/app/components/atoms/textarea.tsx
@@ -14,7 +14,10 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, label, helperText, error, required, ...props }, ref) => {
     return (
       <div className="flex flex-col gap-1.5 w-full">
-        <h4 className="flex gap-2 font-medium">{label}{required && <span className='text-red-500'>*</span>}</h4>
+        <h4 className="flex gap-2 font-medium">
+          {label}
+          {required && <span className="text-red-500">*</span>}
+        </h4>
         <textarea
           className={cn(
             'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
