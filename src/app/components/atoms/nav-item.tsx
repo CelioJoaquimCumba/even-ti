f2b0f089d80e65cc7e6442ff664711612bbcacad
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-09-23 20:19:08 +0200

    feat: disabled non-implemented features

--- src/app/components/atoms/nav-item.tsx
+++ src/app/components/atoms/nav-item.tsx
@@ -10,6 +10,7 @@ interface NavItemProps {
   path: string
   onClick?: () => void
   icon?: React.FC<LucideProps>
+  comingSoon?: boolean
 }
 type state = 'selected' | 'hover' | 'default'
 export default function NavItem({
@@ -18,8 +19,22 @@ export default function NavItem({
   path = '/',
   onClick,
   icon,
+  comingSoon = false,
 }: NavItemProps) {
   const [state, setState] = useState<state>(selected ? 'selected' : 'default')
+  if (comingSoon) {
+    return (
+      <div className="flex justify-between items-center px-6 py-3 rounded-lg cursor-pointer gap-3 bg-muted">
+        <div className="flex gap-3">
+          {icon && <Icon icon={icon} />}
+          {label}
+        </div>
+        <span className="bg-orange-500 text-white p-1 rounded-md text-xs text-center truncate whitespace-nowrap overflow-ellipsis">
+          Coming soon
+        </span>
+      </div>
+    )
+  }
   return (
     <Link
       href={path}
